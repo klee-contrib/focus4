9203a4b38639792951e1ac91cd2786e706cdb466
Damien Frikha <damien.frikha@outlook.com>
2016-08-14 17:01:53 +0200

    TestGen: Ajout instantiation avec props obligatoires non typées

--- src/testing/test-generator.ts
+++ src/testing/test-generator.ts
@@ -7,17 +7,21 @@ const imports =
 `import * as React from "react";
 import {test, dum} from "./src/testing/base-test";`;
 
-interface TestedComponent {
+interface NamedComponent {
     fileName: string;
     importName?: string;
     name?: string;
+}
+
+interface TestedComponent extends NamedComponent {
+    props: ts.Symbol[];
 }
 
 function isClass(node: ts.Node): node is ts.ClassDeclaration {
     return node.kind === ts.SyntaxKind.ClassDeclaration;
 }
 
-function getName(node: ts.Node, type: ts.Type): TestedComponent {
+function getName(node: ts.Node, type: ts.Type): NamedComponent {
     const symbolName = type.symbol && type.symbol.name;
     const fileName = node.getSourceFile().fileName;
     if (symbolName === "default") {
@@ -42,25 +46,33 @@ glob("src/**/*.tsx", (error, fileNames) => {
     const program = ts.createProgram(fileNames, {target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS});
     const checker = program.getTypeChecker();
 
-    let output: {name?: string, importName?: string}[] = [];
+    const output: TestedComponent[] = [];
 
     for (const sourceFile of program.getSourceFiles()) {
         ts.forEachChild(sourceFile, visit);
     }
 
+    const sortedOutput = output.sort((a, b) => a.name > b.name ? 1 : -1);
+
     fs.writeFileSync("tests_generated.tsx",
 `${imports}
 
-${output.sort((a, b) => a.name > b.name ? 1 : -1).map(getImport).join("\n")}
+${sortedOutput.map(getImport).join("\r\n")}
+
+${sortedOutput.map(c => `test("${c.name}", <${c.name} ${c.props.map(p => `${p.getName()}={dum.any}`).join(" ")} />);`).join("\r\n")}
 `);
 
     function visit(node: ts.Node) {
         if (isClass(node) && (!node.modifiers || !node.modifiers.find(m => m.kind === ts.SyntaxKind.AbstractKeyword))) {
             const type = checker.getTypeAtLocation(node);
             if (type && type.getProperties().filter(prop => prop.name === "render").length) {
-                const name = getName(node, type);
-                if (name.name !== "ElementClass") {
-                    output.push(name);
+                const c = getName(node, type) as TestedComponent;
+                c.props = checker.getTypeAtLocation(node.heritageClauses![0].types![0].typeArguments![0])
+                    .getProperties()
+                    .filter(p => !(p.valueDeclaration as ts.ParameterDeclaration).questionToken)
+                    .sort((a, b) => a.getName() > b.getName() ? 1 : -1);
+                if (c.name !== "ElementClass") {
+                    output.push(c);
                 }
             }
         }
