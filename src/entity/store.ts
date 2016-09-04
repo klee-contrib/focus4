ace34148d8d5ac741f98864d73e3c847439763f1
Damien Frikha <damien.frikha@outlook.com>
2016-09-04 14:03:51 +0200

    MobX: EntityStore - ajout de "set" pour objets, sous objets et arrays simple

--- src/entity/store.ts
+++ src/entity/store.ts
@@ -1,4 +1,4 @@
-import {isArray, mapValues} from "lodash";
+import {isArray, isObject, mapValues} from "lodash";
 import {observable} from "mobx";
 
 import {Entity, EntityField} from "./types";
@@ -8,7 +8,11 @@ export interface EntityValue<T> {
     value: T;
 }
 
-export type EntityConfig = {[key: string]: {[field: string]: EntityValue<{}>} | [{[field: string]: EntityValue<{}>}, string]};
+export type EntityConfig = {[key: string]:
+    {[field: string]: EntityValue<{}>}
+    | [{[field: string]: EntityValue<{}>}, string]
+    | [{[field: string]: EntityValue<{}>}[], string]
+};
 
 /**
  * Construit un store d'entité à partir de la config et les entités données.
@@ -16,7 +20,7 @@ export type EntityConfig = {[key: string]: {[field: string]: EntityValue<{}>} | 
  * @param config L'objet constituant du store, dont chaque propriété doit correspondre à une entité. Les valeurs possibles sont "{}", [{}, "entityName"] ou [[], "entityName"] (pour une liste).
  * @param entityList La liste des toutes les entités utilisées par les propriétés du store (y compris les composées).
  */
-export function makeEntityStore<T extends EntityConfig>(config: EntityConfig, entityList: Entity[]): T {
+export function makeEntityStore<T>(config: EntityConfig, entityList: Entity[]): T & {set: (config: {}) => void} {
     const entityMap = entityList.reduce((entities, entity) => {
         entities[entity.name] = entity;
         return entities;
@@ -31,14 +35,18 @@ export function makeEntityStore<T extends EntityConfig>(config: EntityConfig, en
     const entityStore: {[key: string]: any} = {};
 
     for (const ref in config) {
-        entityStore[`${ref}`] = buildEntityEntry(config, entityMap, ref);
+        entityStore[ref] = buildEntityEntry(entityStore, config, entityMap, ref);
     }
 
-    return observable(entityStore) as any as T;
+    entityStore["set"] = fillEntityEntry(entityStore);
+
+    return observable(entityStore) as any;
 }
 
-function buildEntityEntry(config: EntityConfig, entityMap: {[name: string]: Entity}, ref: string): {[key: string]: EntityValue<any>} | any[] {
+function buildEntityEntry(entityStore: {[key: string]: any}, config: EntityConfig, entityMap: {[name: string]: Entity}, ref: string): {[key: string]: EntityValue<any>} | any[] {
     if (isArray(config[ref]) && isArray((config[ref] as any[])[0])) {
+        entityStore["$arrayEntities"] = entityStore["$arrayEntities"] || {};
+        entityStore["$arrayEntities"][ref] = entityMap[(config[ref] as any)[1]];
         return [];
     }
 
@@ -49,7 +57,44 @@ function buildEntityEntry(config: EntityConfig, entityMap: {[name: string]: Enti
         }
         return  {
             $field: entityMap[trueRef].fields[key!],
-            value: v.entityName && buildEntityEntry(config, entityMap, v.entityName)
+            value: v.entityName && buildEntityEntry(entityStore, config, entityMap, v.entityName)
         };
     });
+}
+
+function fillEntityEntry(entityStore: {[key: string]: any}) {
+    return (setConfig: {[key: string]: any}) => {
+        for (const ref in setConfig) {
+            if (!entityStore[ref]) {
+                throw new Error(`"${ref}" n'existe pas dans ce store.`);
+            }
+            if (isArray(setConfig[ref])) {
+               entityStore[ref].replace(setConfig[ref].map((item: {}) => buildEntityEntry(entityStore, {}, {[ref]: entityStore["$arrayEntities"][ref]}, ref)));
+               for (let i = 0; i < setConfig[ref].length; i++) {
+                    fillSubEntityEntry(ref, entityStore["$arrayEntities"][ref].name, entityStore[ref][i], setConfig[ref][i]);
+               }
+            } else {
+                for (const item in setConfig[ref]) {
+                    if (!entityStore[ref][item]) {
+                        throw new Error(`"${ref}" n'a pas de propriété "${item}".`);
+                    }
+                    entityStore[ref][item].value = isObject(setConfig[ref][item]) ? fillSubEntityEntry(ref, item, entityStore[ref][item].value, setConfig[ref][item]) : setConfig[ref][item];
+                }
+            }
+        }
+    };
+}
+
+function fillSubEntityEntry(ref: string, item: string, subEntity: {[key: string]: any}, value: {[key: string]: any}) {
+    if (!isObject(subEntity)) {
+        throw new Error(`La propriété "${item}" de "${ref}" n'est pas un objet.`);
+    }
+
+    for (const key in value) {
+        if (!subEntity[key]) {
+            throw new Error(`La propriété "${key}" de "${item}" n'existe pas.`);
+        }
+        subEntity[key].value = isObject(value[key]) ? fillSubEntityEntry(ref, item, subEntity[key].value, value[key]) : value[key];
+    }
+    return subEntity;
 }
