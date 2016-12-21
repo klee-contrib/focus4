55f7b9d8ff726e3969853c8149e255cffd4b987f
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2016-12-21 10:50:11 +0100

    Simplifcations fetch (utilise content-type)

--- src/network/fetch.ts
+++ src/network/fetch.ts
@@ -1,4 +1,5 @@
 import fetch from "isomorphic-fetch";
+import {isObject} from "lodash";
 import {v4} from "uuid";
 
 import {messageStore} from "../message";
@@ -6,25 +7,23 @@ import {messageStore} from "../message";
 import {ManagedErrorResponse, manageResponseErrors} from "./error-parsing";
 import {requestStore} from "./store";
 
-type DataType = "json" | "string";
-
-async function coreFetch<RS>(url: string, method: string, responseType: DataType, data?: any, contentType?: DataType): Promise<any> {
+export async function coreFetch<RQ, RS>(method: "GET" | "POST" | "PUT" | "DELETE", url: string, data?: RQ): Promise<any> {
     const body = data ? JSON.stringify(data) : undefined;
-    const headers = contentType && data ? {"Content-Type": contentType === "json" ? "application/json" : "text/plain"} : undefined;
+    const headers = data ? {"Content-Type": isObject(data) ? "application/json" : "text/plain"} : undefined;
     const id = v4();
     requestStore.updateRequest({id, url, status: "pending"});
     try {
         const response = await fetch(url, {method, body, headers, credentials: "include"});
         if (response.status >= 200 && response.status < 300) {
             requestStore.updateRequest({id, url, status: "success"});
-            if (responseType === "json") {
+            if (response.headers.get("Content-Type").includes("application/json")) {
                 return await response.json<RS>();
             } else {
                 return await response.text();
             }
         } else {
             requestStore.updateRequest({id, url, status: "error"});
-            if (response.status === 500) {
+            if (response.headers.get("Content-Type").includes("application/json")) {
                 return Promise.reject<ManagedErrorResponse>(manageResponseErrors(await response.json()));
             } else {
                 const errorMessage = `${response.status} error when calling ${url}`;
@@ -42,30 +41,18 @@ async function coreFetch<RS>(url: string, method: string, responseType: DataType
     }
 }
 
-export async function httpDelete(url: string, responseType: "string"): Promise<string>;
-export async function httpDelete<RS>(url: string, responseType?: "json"): Promise<RS>;
-export async function httpDelete(url: string, responseType: DataType = "json") {
-    return coreFetch(url, "DELETE", responseType);
+export async function httpDelete<RS>(url: string): Promise<RS> {
+    return coreFetch("DELETE", url);
 }
 
-export async function httpGet(url: string, responseType: "string"): Promise<string>;
-export async function httpGet<RS>(url: string, responseType?: "json"): Promise<RS>;
-export async function httpGet(url: string, responseType: DataType = "json") {
-    return coreFetch(url, "GET", responseType);
+export async function httpGet<RS>(url: string): Promise<RS> {
+    return coreFetch("GET", url);
 }
 
-export async function httpPost(url: string, data: string, responseType: "string", contentType: "string"): Promise<string>;
-export async function httpPost<RQ>(url: string, data: RQ, responseType: "string", contentType?: "json"): Promise<string>;
-export async function httpPost<RS>(url: string, data: string, responseType: "json", contentType: "string"): Promise<RS>;
-export async function httpPost<RQ, RS>(url: string, data: RQ, responseType?: "json", contentType?: "json"): Promise<RS>;
-export async function httpPost(url: string, data: any, responseType: DataType = "json", contentType: DataType = "json") {
-    return coreFetch(url, "POST", responseType, data, contentType);
+export async function httpPost<RQ, RS>(url: string, data: RQ): Promise<RS> {
+    return coreFetch("POST", url, data);
 }
 
-export async function httpPut(url: string, data: string, responseType: "string", contentType: "string"): Promise<string>;
-export async function httpPut<RQ>(url: string, data: RQ, responseType: "string", contentType?: "json"): Promise<string>;
-export async function httpPut<RS>(url: string, data: string, responseType: "json", contentType: "string"): Promise<RS>;
-export async function httpPut<RQ, RS>(url: string, data: RQ, responseType?: "json", contentType?: "json"): Promise<RS>;
-export async function httpPut(url: string, data: any, responseType: DataType = "json", contentType: DataType = "json") {
-    return coreFetch(url, "PUT", responseType, data, contentType);
+export async function httpPut<RQ, RS>(url: string, data: RQ): Promise<RS> {
+    return coreFetch("PUT", url, data);
 }
