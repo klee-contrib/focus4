714d9dd3bbeaa2df1c12f310646b3017cce687a4
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2017-03-22 18:15:01 +0100

    `injectByName(name)` et `injectByPropName`

--- src/ioc.ts
+++ src/ioc.ts
@@ -2,14 +2,24 @@ import {Container} from "inversify";
 import getDecorators from "inversify-inject-decorators";
 import "reflect-metadata";
 
+/** Container pour l'injection de dépendances. */
 export const container = new Container();
 
 const {lazyInject} = getDecorators(container);
 
-export function inject(name: string) {
+/**
+ * Décorateur pour injecter un composant/service dans une propriété de classe.
+ * @param name Le nom du binding.
+ */
+export function injectByName(name: string) {
     return lazyInject(name);
 }
 
-export function resolve(name: string) {
-    return container.get(name);
+/**
+ * Décorateur pour injecter un composant/service dans une propriété de classe.
+ *
+ * Le nom de la propriété est utilisé comme nom du binding.
+ */
+export function injectByPropName(target: Object, propertyKey: string) {
+    lazyInject(propertyKey)(target, propertyKey);
 }
