f7cdb7a4436f83cba29a4204c33392d74426d033
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2017-03-23 17:14:21 +0100

    Ajout @classWhen

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import {autorun, IReactionOptions, reaction} from "mobx";
+import {autorun, IReactionOptions, reaction, when} from "mobx";
 
 /** Classe React. */
 export type RCL = React.ComponentLifecycle<any, any> & {[key: string]: any};
@@ -6,21 +6,12 @@ export type RCL = React.ComponentLifecycle<any, any> & {[key: string]: any};
 /** Type d'expressions possibles pour le décorateur de réaction. */
 export type ReactionExpression<T> = ((inst: T) => () => {}) | (() => {});
 
+/** Type d'expressions possibles pour le décorateur de `when` */
+export type WhenExpression<T> = ((inst: T) => () => boolean) | (() => boolean);
+
 /** Décorateur permettant, dans une classe React, de poser un autorun sur la fonction décorée. */
 export function classAutorun(target: RCL, propertyKey: keyof RCL, _: TypedPropertyDescriptor<any>) {
-    function componentWillMount(this: RCL) {
-        this[`$autorun_${propertyKey}`] = autorun((target[propertyKey]).bind(this));
-    }
-
-    function componentWillUnmount(this: RCL) {
-        this[`$autorun_${propertyKey}`]();
-    }
-
-    const baseCWM = target.componentWillMount;
-    const baseCWUM = target.componentWillUnmount;
-
-    target.componentWillMount = !baseCWM ? componentWillMount : function(this: RCL) { componentWillMount.apply(this); baseCWM.apply(this); };
-    target.componentWillUnmount = !baseCWUM ? componentWillUnmount : function(this: RCL) { componentWillUnmount.apply(this); baseCWUM.apply(this); };
+    patchClass("autorun", target, propertyKey);
 }
 
 /**
@@ -30,23 +21,44 @@ export function classAutorun(target: RCL, propertyKey: keyof RCL, _: TypedProper
  */
 export function classReaction<T extends RCL>(expression: ReactionExpression<T>, opts?: IReactionOptions) {
     return function(instance: RCL, propertyKey: keyof RCL) {
-        function componentWillMount(this: T) {
-            this[`$reaction_${propertyKey}`] = reaction(hasExpressionThis(expression) ? expression(this) : expression, instance[propertyKey].bind(this), opts);
-        }
-
-        function componentWillUnmount(this: RCL) {
-            this[`$reaction_${propertyKey}`]();
-        }
-
-        const baseCWM = instance.componentWillMount;
-        const baseCWUM = instance.componentWillUnmount;
-
-        instance.componentWillMount = !baseCWM ? componentWillMount : function(this: RCL) { componentWillMount.apply(this); baseCWM.apply(this); };
-        instance.componentWillUnmount = !baseCWUM ? componentWillUnmount : function(this: RCL) { componentWillUnmount.apply(this); baseCWUM.apply(this); };
+        patchClass("reaction", instance, propertyKey, expression, opts);
     };
 }
 
+/**
+ * Décorateur permettant, dans une classe React, de poser un `when` sur la fonction décorée.
+ * @param expression L'expression à tracker pour le when. Si le contexte est nécessaire, le passer dans un lambda englobant.
+ */
+export function classWhen<T extends RCL>(expression: WhenExpression<T>) {
+    return function(instance: RCL, propertyKey: keyof RCL) {
+        patchClass("when", instance, propertyKey, expression);
+    };
+}
+
+function patchClass<T extends RCL>(type: "autorun" | "reaction" | "when", instance: T, propertyKey: keyof RCL, expression?: WhenExpression<T> | ReactionExpression<T>, opts?: IReactionOptions) {
+    function componentWillMount(this: T) {
+        this[`${type}_${propertyKey}`] =
+            type === "autorun" ?
+                autorun((instance[propertyKey]).bind(this))
+          : type === "reaction" ?
+                reaction(hasExpressionThis(expression) ? expression(this) : expression as any, instance[propertyKey].bind(this), opts)
+          : type === "when" ?
+                when(hasExpressionThis(expression) ? expression(this) : expression as any, instance[propertyKey].bind(this))
+          : undefined;
+    }
+
+    function componentWillUnmount(this: RCL) {
+        this[`${type}_${propertyKey}`]();
+    }
+
+    const baseCWM = instance.componentWillMount;
+    const baseCWUM = instance.componentWillUnmount;
+
+    instance.componentWillMount = !baseCWM ? componentWillMount : function(this: RCL) { componentWillMount.apply(this); baseCWM.apply(this); };
+    instance.componentWillUnmount = !baseCWUM ? componentWillUnmount : function(this: RCL) { componentWillUnmount.apply(this); baseCWUM.apply(this); };
+}
+
 /** Permet de distinguer le type d'expression fourni à la réaction. */
-function hasExpressionThis<T extends RCL>(expression: ReactionExpression<T>): expression is (inst: T) => () => {} {
-    return typeof (expression as ((inst: T) => () => {}))({} as any) === "function";
+function hasExpressionThis<T extends RCL>(expression?: ReactionExpression<T> | WhenExpression<T>): expression is (inst: T) => () => {} {
+    return typeof (expression as ((inst: T) => () => any))({} as any) === "function";
 }
