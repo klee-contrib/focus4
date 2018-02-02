6ae63b3943a3d996e96a6bd04585df8b341c1e9c
Damien Frikha <damien.frikha@kleegroup.com>
2018-02-02 18:45:50 +0100

    Fix #52 (8.7.1)

--- src/network/loading-bar.tsx
+++ src/network/loading-bar.tsx
@@ -2,30 +2,34 @@ import {observer} from "mobx-react";
 import * as React from "react";
 import {themr} from "react-css-themr";
 import {FontIcon} from "react-toolbox/lib/font_icon";
+import {ProgressBar, ProgressBarTheme} from "react-toolbox/lib/progress_bar";
 
 import {getIcon} from "../components";
-import {ReactComponent} from "../config";
 
 import {requestStore} from "./store";
 
 import * as styles from "./__style__/loading-bar.css";
 
-export type LoadingBarStyle = Partial<typeof styles>;
+export type LoadingBarStyle = Partial<typeof styles> & ProgressBarTheme;
 
 export interface LoadingBarProps {
+    /** Affiche la barre de dev qui montre l'état du RequestStore. */
     displayDevBar?: boolean;
+    /** Pour les icônes. Par défaut : "focus". */
     i18nPrefix?: string;
-    ProgressBar: ReactComponent<{completed: number}>;
+    /** Type de la ProgressBar. Par défaut : "linear". */
+    progressBarType?: "linear" | "circular";
+    /** CSS. */
     theme?: LoadingBarStyle;
 }
 
 /** Composant standard pour afficher une barre de chargement sur l'état des requêtes en cours. */
-export const LoadingBar = observer(({i18nPrefix = "focus", theme, displayDevBar, ProgressBar}: LoadingBarProps) => {
+export const LoadingBar = observer(({i18nPrefix = "focus", progressBarType, theme, displayDevBar}: LoadingBarProps) => {
     const {count, error, pending, success} = requestStore;
     const completed = +((count.total - count.pending) / count.total) * 100;
     return (
         <div className={theme!.bar}>
-            {completed < 100 ? <ProgressBar completed={completed} /> : null}
+            {completed < 100 ? <ProgressBar type={progressBarType} theme={theme} /> : null}
             {displayDevBar ?
                 <ul>
                     <li><FontIcon>{getIcon(`${i18nPrefix}.icons.loadingBar.pending`)}</FontIcon> pending {pending}</li>
