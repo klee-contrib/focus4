259062001a434ec005102d7a82143aa582a145cb
JabX <damien.frikha@outlook.com>
2018-07-18 19:42:50 +0200

    Mise au propre radio-select + renames

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export {default as Autocomplete, AutocompleteProps, AutocompleteStyle} from "./a
 export {default as ButtonBackToTop, ButtonBackToTopProps, ButtonBackToTopStyle} from "./button-back-to-top";
 export {default as ButtonMenu, IconMenu, MenuItem} from "./button-menu";
 export {default as Checkbox, CheckboxProps} from "./checkbox";
-export {default as CheckboxSelect, CheckboxSelectProps, CheckboxSelectStyle} from "./checkbox-select";
 export {default as Display, DisplayProps, DisplayStyle} from "./display";
 export {getIcon} from "./icon";
 export {default as Input, InputProps} from "./input";
@@ -13,4 +12,6 @@ export {default as Panel, PanelButtons, PanelProps, PanelStyle} from "./panel";
 export {default as Popin, PopinStyle} from "./popin";
 export {default as ScrollspyContainer, ScrollspyContainerProps, ScrollspyStyle} from "./scrollspy-container";
 export {default as Select, SelectProps} from "./select";
+export {default as SelectCheckbox, SelectCheckboxProps, SelectCheckboxStyle} from "./select-checkbox";
+export {default as SelectRadio, SelectRadioProps, SelectRadioStyle} from "./select-radio";
 export {default as Switch, SwitchProps} from "./switch";
---
+++ src/components/select-radio.tsx
@@ -0,0 +1,97 @@
+import i18next from "i18next";
+import React from "react";
+import {themr} from "react-css-themr";
+import {RadioButton, RadioGroup} from "react-toolbox/lib/radio";
+
+import * as styles from "./__style__/select-radio.css";
+export type SelectRadioStyle = Partial<typeof styles>;
+
+/** Props for RadioSelect */
+export interface SelectRadioProps {
+    /** Disabled radio-select, default to: false */
+    disabled?: boolean;
+    /** Error message to display. */
+    error?: string;
+    /** Label to display. */
+    label?: string;
+    /** Name of field for label. */
+    labelKey: string;
+    /** Name for input field. */
+    name: string;
+    /** Call with each value change. */
+    onChange: (value: string | number | undefined) => void;
+    /** CSS. */
+    theme?: SelectRadioStyle;
+    /** Value. */
+    value?: string | number | undefined;
+    /** Name of field for key. */
+    valueKey: string;
+    /** Values. */
+    values: {}[];
+    /** If has undefined, default to: false */
+    hasUndefined?: boolean;
+    /** Undefined label */
+    undefinedLabel?: string;
+    /** Undefined position, default to: bottom */
+    undefinedPosition?: "top" | "bottom";
+}
+
+/** RadioSelect component */
+export function SelectRadio({
+    disabled = false,
+    error,
+    label,
+    labelKey,
+    name,
+    onChange,
+    theme,
+    value,
+    valueKey,
+    values,
+    hasUndefined = false,
+    undefinedLabel = "focus.select.none",
+    undefinedPosition = "bottom"
+}: SelectRadioProps) {
+    let definitiveValues = values;
+    if (hasUndefined && undefinedPosition === "bottom") {
+        definitiveValues = [
+            ...values,
+            {[valueKey]: undefined, [labelKey]: undefinedLabel}
+        ];
+    }
+    if (hasUndefined && undefinedPosition === "top") {
+        definitiveValues = [
+            {[valueKey]: undefined, [labelKey]: undefinedLabel},
+            ...values
+        ];
+    }
+
+    return (
+        <div className={theme!.select}>
+            {label && <h5 className={theme!.title}>{i18next.t(label)}</h5>}
+            <RadioGroup
+                name={name}
+                value={value}
+                onChange={onChange}
+                disabled={disabled}
+            >
+                {definitiveValues.map(option => {
+                    const optVal = (option as any)[valueKey];
+                    const optLabel = (option as any)[labelKey];
+
+                    return (
+                        <RadioButton
+                            key={optVal || "undefined"}
+                            label={i18next.t(optLabel)}
+                            value={optVal}
+                            theme={{field: theme!.option}}
+                        />
+                    );
+                })}
+            </RadioGroup>
+            {error ? <div>{error}</div> : null}
+        </div>
+    );
+}
+
+export default themr("selectRadio", styles)(SelectRadio);
