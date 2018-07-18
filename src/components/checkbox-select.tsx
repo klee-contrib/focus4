b73761ca2bd9a6b9ccb37a1324f802dbb2fe9262
JabX <damien.frikha@outlook.com>
2018-07-18 19:27:18 +0200

    Mise en forme CheckboxSelect.

--- src/components/checkbox-select.tsx
+++ src/components/checkbox-select.tsx
@@ -1,109 +1,113 @@
-// Libs
-import i18n from "i18next";
+import i18next from "i18next";
 import React from "react";
+import {themr} from "react-css-themr";
 
-// Components
-import { Checkbox } from "./checkbox";
+import {Checkbox} from "./checkbox";
 
-// Styles
-import "./__style__/checkbox-select.scss";
+import * as styles from "./__style__/checkbox-select.css";
+export type CheckboxSelectStyle = Partial<typeof styles>;
 
 function clickHandlerFactory(
-  isDisabled: boolean,
-  isSelected: boolean,
-  value: string[] | number[] | undefined,
-  optVal: string | number,
-  onChange: (value: string[] | number[] | undefined) => void
+    isDisabled: boolean,
+    isSelected: boolean,
+    value: string[] | number[] | undefined,
+    optVal: string | number,
+    onChange: (value: string[] | number[] | undefined) => void
 ) {
-  return (e: React.SyntheticEvent<any>) => {
-    e.stopPropagation();
-    e.preventDefault();
+    return (e: React.SyntheticEvent<any>) => {
+        e.stopPropagation();
+        e.preventDefault();
 
-    if (!isDisabled) {
-      if (isSelected) {
-        // is selected -> remove it
-        onChange(
-          value
-            ? (value as any).filter((val: any) => val !== optVal)
-            : undefined
-        );
-      } else {
-        // is not selected -> add it
-        onChange((value ? [...value, optVal] : [optVal]) as any);
-      }
-    }
-  };
+        if (!isDisabled) {
+            if (isSelected) {
+                // is selected -> remove it
+                onChange(
+                    value
+                    ? (value as any).filter((val: any) => val !== optVal)
+                    : undefined
+                );
+            } else {
+                // is not selected -> add it
+                onChange((value ? [...value, optVal] : [optVal]) as any);
+            }
+        }
+    };
 }
 
 export interface CheckboxSelectProps {
-  /** Disabled checkbox-select. */
-  disabled?: boolean;
-  /** Error message to display. */
-  error?: string;
-  /** Label to display. */
-  label?: string;
-  /** Name of field for label. */
-  labelKey: string;
-  /** Name for input field. */
-  name: string;
-  /** Call with each value change. */
-  onChange: (value: string[] | number[] | undefined) => void;
-  /** Value. */
-  value?: string[] | number[];
-  /** Name of field for key. */
-  valueKey: string;
-  /** Values. */
-  values: {}[];
+    /** Disabled checkbox-select. */
+    disabled?: boolean;
+    /** Error message to display. */
+    error?: string;
+    /** Label to display. */
+    label?: string;
+    /** Name of field for label. */
+    labelKey: string;
+    /** Name for input field. */
+    name: string;
+    /** Call with each value change. */
+    onChange: (value: string[] | number[] | undefined) => void;
+    /** CSS. */
+    theme?: CheckboxSelectStyle;
+    /** Value. */
+    value?: string[] | number[];
+    /** Name of field for key. */
+    valueKey: string;
+    /** Values. */
+    values: {}[];
 }
 
 export function CheckboxSelect({
-  disabled = false,
-  error,
-  label,
-  labelKey,
-  name,
-  onChange,
-  value,
-  valueKey,
-  values
+    disabled = false,
+    error,
+    label,
+    labelKey,
+    name,
+    onChange,
+    theme,
+    value,
+    valueKey,
+    values
 }: CheckboxSelectProps) {
-  return (
-    <div className="c-checkbox-select">
-      {label && <h5 className="c-checkbox-select__title">{i18n.t(label)}</h5>}
-      <ul>
-        {values.map(option => {
-          const optVal = (option as any)[valueKey];
-          const optLabel = (option as any)[labelKey];
+    return (
+        <div className={theme!.select}>
+            {label && <h5>{i18next.t(label)}</h5>}
+            <ul>
+                {values.map(option => {
+                    const optVal = (option as any)[valueKey];
+                    const optLabel = (option as any)[labelKey];
 
-          const isSelected = value
-            ? !!(value as any).find((val: any) => optVal === val)
-            : false;
-          const clickHandler = clickHandlerFactory(
-            disabled,
-            isSelected,
-            value,
-            optVal,
-            onChange
-          );
+                    const isSelected = value
+                        ? !!(value as any).find((val: any) => optVal === val)
+                        : false;
+                    const clickHandler = clickHandlerFactory(
+                        disabled,
+                        isSelected,
+                        value,
+                        optVal,
+                        onChange
+                    );
 
-          return (
-            <li
-              key={optVal}
-              onClick={clickHandler}
-              className="c-checkbox-select__option"
-            >
-              <Checkbox
-                name={`${name}-${optVal}`}
-                value={isSelected}
-                onClick={clickHandler}
-                disabled={disabled}
-                label={i18n.t(optLabel)}
-              />
-            </li>
-          );
-        })}
-      </ul>
-      {error ? <div>{error}</div> : null}
-    </div>
-  );
+                    return (
+                        <li
+                            key={optVal}
+                            onClick={clickHandler}
+                            className={theme!.option}
+                        >
+                            <Checkbox
+                                name={`${name}-${optVal}`}
+                                value={isSelected}
+                                onClick={clickHandler}
+                                disabled={disabled}
+                                label={i18next.t(optLabel)}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+            {error ? <div>{error}</div> : null}
+        </div>
+    );
 }
+
+export default themr("checkboxSelect", styles)(CheckboxSelect);
