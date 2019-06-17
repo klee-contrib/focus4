b655266b1214cb27f7ec9507919f27d0b69a1eca
Damien Frikha <damien.frikha@kleegroup.com>
2019-06-17 20:02:41 +0200

    Fix typage storeTableFor

--- src/collections/components/list/table.tsx
+++ src/collections/components/list/table.tsx
@@ -10,17 +10,18 @@ import {ListBase, ListBaseProps} from "./list-base";
 import * as styles from "./__style__/list.css";
 const Theme = themr("list", styles);
 
+/** Colonne de tableau. */
+export interface TableColumn<T> {
+    /** Classe CSS pour la colonne. */
+    className?: string;
+    /** Contenu de la colonne. */
+    content: (data: T) => React.ReactNode;
+    /** Libellé du titre de la colonne. */
+    title: string;
+}
+
 /** Props du tableau de base. */
 export interface TableProps<T> extends ListBaseProps<T> {
-    /** La description des colonnes du tableau. */
-    columns: {
-        /** Classe CSS pour la colonne. */
-        className?: string;
-        /** Contenu de la colonne. */
-        content: (data: T) => React.ReactNode;
-        /** Libellé du titre de la colonne. */
-        title: string;
-    }[];
     /** Classe CSS pour une ligne. */
     lineClassName?: (data: T) => string;
     /** Appelé au clic sur une ligne. */
@@ -29,7 +30,10 @@ export interface TableProps<T> extends ListBaseProps<T> {
 
 /** Tableau standard */
 @observer
-export class Table<T, P extends TableProps<T> = TableProps<T> & {data: T[]}> extends ListBase<T, P> {
+export class Table<
+    T,
+    P extends TableProps<T> = TableProps<T> & {data: T[]; columns: TableColumn<T>[]}
+> extends ListBase<T, P> {
     /** Les données. */
     protected get data() {
         return (this.props as any).data || [];
@@ -40,7 +44,7 @@ export class Table<T, P extends TableProps<T> = TableProps<T> & {data: T[]}> ext
         return (
             <thead>
                 <tr>
-                    {this.props.columns.map(({title}) => (
+                    {((this.props as any).columns as TableColumn<T>[]).map(({title}) => (
                         <th key={title}>{i18next.t(title)}</th>
                     ))}
                 </tr>
@@ -54,7 +58,7 @@ export class Table<T, P extends TableProps<T> = TableProps<T> & {data: T[]}> ext
         const {clickable} = useTheme("list", styles, theme);
         return useObserver(() => (
             <tr ref={ref} className={`${lineClassName ? lineClassName(data) : ""} ${onLineClick ? clickable : ""}`}>
-                {this.props.columns.map(({className, content}, idx) => (
+                {((this.props as any).columns as TableColumn<T>[]).map(({className, content}, idx) => (
                     <td className={className} key={idx} onClick={() => (onLineClick ? onLineClick(data) : undefined)}>
                         {content(data)}
                     </td>
@@ -105,6 +109,6 @@ export class Table<T, P extends TableProps<T> = TableProps<T> & {data: T[]}> ext
  * Crée un composant de tableau standard.
  * @param props Les props du tableau.
  */
-export function tableFor<T>(props: TableProps<T> & {data: T[]}) {
+export function tableFor<T>(props: TableProps<T> & {data: T[]; columns: TableColumn<T>[]}) {
     return <Table<T> {...props} />;
 }
