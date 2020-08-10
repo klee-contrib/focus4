157418e98179d1cb2bcedc2337cda86beee503b6
Damien Frikha <damien.frikha@kleegroup.com>
2020-08-11 00:12:21 +0200

    router.switch + RouterConstraintBuilder

--- packages/core/src/router/next.ts
+++ packages/core/src/router/next.ts
@@ -50,7 +50,18 @@ type Router<T> = ViewStore<T> & {
     is(predicate: (x: YoloDab<T>) => void): boolean;
     to(predicate: (x: YoloDoubleDab<T>) => void): void;
     sub<S>(predicate: (x: YoloDab<T>) => YoloDab<S>): Router<S>;
+    switch<S, R>(predicate: (x: YoloDab<T>) => YoloDab<S>, switcher: (x: keyof S | undefined) => R): R;
 };
+
+interface RouterConstraintBuilder<T> {
+    block(is: (x: YoloDab<T>) => void, condition: () => boolean): RouterConstraintBuilder<T>;
+    redirect(
+        is: (x: YoloDab<T>) => void,
+        condition: () => boolean,
+        to: (x: YoloDoubleDab<T>) => void
+    ): RouterConstraintBuilder<T>;
+    sub<S>(predicate: (x: YoloDab<T>) => YoloDab<S>): RouterConstraintBuilder<S>;
+}
 
 const p = {
     number<T extends number>(required = false): NumberParam<T> {
@@ -76,7 +87,7 @@ function t() {
     /** */
 }
 
-function makeRouter<T>(config: T): Router<T> {
+function makeRouter<T>(config: T, _builder?: (b: RouterConstraintBuilder<T>) => void): Router<T> {
     const store = buildView(config);
 
     const endpoints: [string, Function][] = [];
@@ -124,8 +135,7 @@ function buildView<T>(config: T): ViewStore<T> {
         }
     } else {
         for (const key in config) {
-            const value = config[key];
-            if (value === undefined || isNumberParam(value) || isStringParam(value)) {
+            if (config[key] === undefined || isNumberParam(config[key]) || isStringParam(config[key])) {
                 (object as any)[key] = undefined;
             } else {
                 (object as any)[key] = buildView(config[key]);
@@ -222,7 +232,35 @@ router.is(a => a("operation")("ofaId")("echeance")("echId")("reglement")("regId"
     Si je veux distinguer entre /operation et /operation/1, je utilise is(a => a("operation")("ofaId"))
     au lieu de vérifier si router.operation.ofaId est undefined.
 
-    Pour naviguer, le routeur dispose d'une méthode "to", qui fonctionne un peu sur le même principe :
+    Le routeur publie également une méthode "switch" qui permet de retourner une valeur différente par section
+    de route qui matche :
+*/
+
+const value = router.switch(
+    a => a("operation")("ofaId"),
+    route => {
+        switch (route) {
+            case "garantie":
+                return 1;
+            case "echeance":
+                return 3;
+            case "pret":
+                return 5;
+            case "tam":
+                return 7;
+            case undefined:
+                return 0;
+        }
+    }
+);
+
+/*
+    Le cas "undefined" est retourné si ni "/operation/:ofaId/garantie" ni "/operation/:ofaId/echeance" ne
+    matche, et également si du coup /operation/:ofaId ne matche pas du tout.
+    ("switch" ne propose pas de version avec callback).
+
+    Pour naviguer, le routeur dispose d'une méthode "to", qui fonctionne un peu sur le même principe que
+    "is" :
 */
 
 router.to(a => a("operation")(1)("tam")("COURANT")(4));
@@ -243,7 +281,7 @@ router.sub(a => a("operation")("ofaId")).to(a => a("garantie")(4));
 
 /*
     Ce sous-routeur dispose de la même API que le routeur principal (is/to/sub), mais il n'expose que
-    les URLs qui à partir de sa racine dans son objet de valeur et dans "is" et "to".
+    les URLs qui à partir de sa racine dans son objet de valeur et dans "is", "switch" et "to".
 
     Les méthodes fonctionnent de la même façon, en complétant les URLs construite avec la partie commune
     définie dans le "sub".
@@ -277,6 +315,58 @@ echRouter.reglement.regId = 1;
     Dans mon exemple j'ai /operation/ofaId/echeance/...., mais je pourrais très bien avoir avec aussi
     /projet/echeance/... ou n'importe quoi d'autre.
 
-    (Remarque de clôture : je l'ai pas dit avant mais évidemment les paramètres doivent tous avoir un
+    (Remarque : je l'ai pas dit avant mais évidemment les paramètres doivent tous avoir un
     nom unique, sinon le matching des paramètres via l'URL ne pourra pas marcher)
+
+    "makeRouter" prend également un deuxième paramètre optionnel qui permet d'ajouter des règles de blocage
+    ou de redirection sur certaines routes si une condition n'est pas respectée :
+*/
+
+const router2 = makeRouter(
+    {
+        projet: {
+            detail: {}
+        },
+        operation: p.param("ofaId", p.number(), {
+            garantie: p.param("gfaId", p.number(true)),
+            pret: p.param("pnfId", p.number(true)),
+            tam: p.param("ttaCode", p.string<"COURANT" | "THEORIQUE">(true), p.param("mprId", p.number())),
+            echeance
+        })
+    },
+    b => {
+        b.block(
+            a => a("operation")("ofaId")("echeance"),
+            () => "lol".length !== 3
+        ).redirect(
+            a => a("projet"),
+            () => "deso".length !== 4,
+            a => a
+        );
+
+        const subBuilder: RouterConstraintBuilder<typeof echeance> = b.sub(a => a("operation")("ofaId")("echeance"));
+        subBuilder.block(
+            a => a("echId")("reglement"),
+            () => "yolo".length !== 4
+        );
+    }
+);
+
+/*
+    "block" matche une route avec "is" et interdira cette route si la condition du callback est remplie.
+    Une route bloquée équivaut à une route inexistante et bloquera la navigation. Une mise à jour forcée
+    de l'URL sur une route inexistante (sans utiliser "to" qui interdira) restaurera l'ancienne URL (et ne
+    fera donc rien). Initialement, la route "/" est active donc si la première route est une route inexistante
+    alors la "redirection" ira à la racine.
+
+    "redirect" fonctionne comme "block", sauf qu'il permet de choisir une route vers laquelle rediriger. Cela
+    peut être une page d'erreur quelconque ou bien simplement la route parente.
+
+    Il est possible de spécifier plusieurs fois la même route, de préférence avec des conditions différentes
+    (pour rediriger vers des endroits différents par exemple). Le routeur exécutera les conditions dans l'ordre
+    dans lequel elle ont été définies et c'est donc la première qui matche qui "l'emportera".
+
+    Enfin, comme pour le routeur en lui même, il est possible de définir des sous-builder pour appliquer des
+    règles sur des sous-sections du routeur (pour les partager pour pour les décrire à côté du routeur s'il est
+    dans un module par exemple).
 */
