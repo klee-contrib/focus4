923067b18cbb31bade034c3e87a355af5d3259bb
Damien <damien.frikha@kleegroup.com>
2022-11-15 17:48:00 +0100

    Utilise process.cwd() dans le cssgen

--- packages/tooling/src/generator/index.ts
+++ packages/tooling/src/generator/index.ts
@@ -1,4 +1,5 @@
 import {promises as fs} from "fs";
+import path from "path";
 
 import Core from "css-modules-loader-core";
 import glob from "glob";
@@ -6,7 +7,8 @@ import {camelCase, sortBy, upperFirst} from "lodash";
 
 export async function generateCSSTypings(rootDir: string) {
     const cssLoader = new Core();
-    const files = glob.sync(`${rootDir}/**/*.css`).map(file => {
+    const root = path.join(process.cwd(), rootDir).replace(/\\/g, "/");
+    const files = glob.sync(`${root}/**/*.css`).map(file => {
         const parts = file.split("/");
         const fileName = parts[parts.length - 1];
         const interfaceName = camelCase(fileName.substring(0, fileName.length - 4));
@@ -15,6 +17,7 @@ export async function generateCSSTypings(rootDir: string) {
     await Promise.all(
         files.map(async ({file, interfaceName}) => {
             const content = await fs.readFile(file);
+            const filePath = file.replace(root, rootDir);
             const {exportTokens} = await cssLoader.load(content.toString());
             const elements = new Set<string>();
             let hasModifier = false;
@@ -41,7 +44,7 @@ export async function generateCSSTypings(rootDir: string) {
 
 ${Array.from(elements)
     .sort()
-    .map(element => `interface ${element} { _${hashCode(file + element)}: void }`)
+    .map(element => `interface ${element} { _${hashCode(filePath + element)}: void }`)
     .join("\r\n")}
 
 export interface ${upperFirst(interfaceName)}Css {
@@ -53,7 +56,7 @@ export default ${interfaceName}Css;
 `;
 
             await fs.writeFile(`${file}.d.ts`, output);
-            console.info(`${file}.d.ts généré.`);
+            console.info(`${filePath}.d.ts généré.`);
         })
     );
 }
