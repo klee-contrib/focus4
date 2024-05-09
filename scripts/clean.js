64a7967dfbe04266e33c5707759b6726dde4b950
Damien <damien.frikha@kleegroup.com>
2024-05-10 00:58:22 +0200

    maj lerna/npm workspace

--- scripts/clean.js
+++ scripts/clean.js
@@ -4,7 +4,8 @@ const {glob} = require("glob");
 const rimraf = require("rimraf");
 
 glob.sync("packages/*/lib").forEach(f => rimraf.sync(f));
+glob.sync("packages/*/node_modules").forEach(f => rimraf.sync(f));
 glob.sync("packages/*/src/**/*.css.d.ts").forEach(f => fs.unlinkSync(f));
 glob.sync("packages/docs/**/metas").forEach(f => rimraf.sync(f));
-glob.sync("docs/components").forEach(f => rimraf.sync(f));
+rimraf.sync("docs");
 rimraf.sync("node_modules");
