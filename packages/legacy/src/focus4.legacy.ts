ff1e622193047e19db4bcdc0bc3b739c3644f9e4
Damien <damien.frikha@kleegroup.com>
2025-02-01 11:16:58 +0100

    mobx-react 9, décorateurs stage 3, retrait module legacy

--- packages/legacy/src/focus4.legacy.ts
+++
@@ -1,6 +0,0 @@
-export {AutoForm, createViewModel, fieldWrapperFor} from "./entity";
-export {classAutorun, classReaction, classWhen} from "./reactions";
-export {ViewStore, makeRouter} from "./router";
-
-export type {AutoFormOptions, ServiceConfig, ViewModel} from "./entity";
-export type {RouterConfig} from "./router";
