29c6c30d1ba096da834a98ed17e1536704dbd562
Damien <damien.frikha@kleegroup.com>
2025-02-01 11:16:58 +0100

    mobx-react 9, décorateurs stage 3, retrait module legacy

--- packages/forms/src/stores/index.ts
+++ packages/forms/src/stores/index.ts
@@ -1,2 +1,2 @@
-export {makeFormActions, makeFormNode, useFormActions, useFormNode} from "./form";
+export {useFormActions, useFormNode} from "./form";
 export {useLoad, useReferenceTracking} from "./load";
