18ef8d2d09e8b6a70a2b923307ae282526af0869
Damien <damien.frikha@kleegroup.com>
2025-02-02 15:11:53 +0100

    Fix type traductions layout meta package focus4

--- packages/focus4/index.d.ts
+++ packages/focus4/index.d.ts
@@ -2,6 +2,4 @@ import {translation as ct} from "@focus4/collections";
 import {translation as ft} from "@focus4/forms";
 import {translation as lt} from "@focus4/layout";
 
-export const translation: typeof ct.fr &
-    typeof ft.fr &
-    typeof lt.fr & {icons: typeof ct.icons & typeof ft.icons & typeof lt.icons};
+export const translation: typeof ct.fr & typeof ft.fr & {icons: typeof ct.icons & typeof ft.icons & typeof lt.icons};
