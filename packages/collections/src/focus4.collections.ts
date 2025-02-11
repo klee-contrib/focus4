e11377293e0524dd75763e39b9e1942c63dd3011
Damien <damien.frikha@kleegroup.com>
2025-02-11 11:25:51 +0100

    `initI18n` et retrait du méta package `focus4`

--- packages/collections/src/focus4.collections.ts
+++ packages/collections/src/focus4.collections.ts
@@ -36,7 +36,7 @@ export {
     shouldDisplayFacet,
     summaryCss
 } from "./search";
-export {translation} from "./translation";
+export {i18nCollections} from "./translation";
 
 export type {
     AddItemProps,
