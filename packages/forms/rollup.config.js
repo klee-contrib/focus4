16074be37a9d3029f5a1c5b1f79443d203694016
Damien <damien.frikha@kleegroup.com>
2025-12-17 13:34:04 +0100

    fix config rollup forms

--- packages/forms/rollup.config.js
+++ packages/forms/rollup.config.js
@@ -1,30 +1,20 @@
 // @ts-check
 import typescript from "@rollup/plugin-typescript";
 
-import {generateCSSTypings} from "@focus4/tooling";
-
 import pkg from "./package.json";
 
-export default (async () => {
-    await generateCSSTypings("src");
+/** @type {import("rollup").RollupOptions} */
+const config = {
+    input: "src/focus4.forms.ts",
+    plugins: [typescript()],
+    treeshake: {
+        moduleSideEffects: false
+    },
+    output: {
+        format: "esm",
+        dir: "lib"
+    },
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {}), "react/jsx-runtime"]
+};
 
-    /** @type {import("rollup").RollupOptions} */
-    const config = {
-        input: "src/focus4.forms.ts",
-        plugins: [typescript()],
-        treeshake: {
-            moduleSideEffects: false
-        },
-        output: {
-            format: "esm",
-            dir: "lib"
-        },
-        external: [
-            ...Object.keys(pkg.dependencies || {}),
-            ...Object.keys(pkg.peerDependencies || {}),
-            "react/jsx-runtime"
-        ]
-    };
-
-    return config;
-})();
+export default config;
